e422cf7db9083c4bbaf09f41d7b6ba9391fbd478
safarmammadov <safarmobile@gmail.com>
2020-05-11 01:22:21 +0300

    commite one

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('hello')
+
+n1 = int(input("Enter number 1: "))
+n2 = int(input("Enter number 2: "))
+
+if n1 > 0 and n2 > 0:
+    div = n1 / n2
+    mult = n1 * n2
+    sum = n1 + n2
+    sub = n1 - n2
+
+    print (f'Division: {div}, multiplication: {mult}, sum: {sum}, substraction: {sub}')
+
+else:
+    print ("Both numbers should be positive")
