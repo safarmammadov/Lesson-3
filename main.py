3fcdf41661d168821ec1834461ef54466ca3abc5
safarmammadov <safarmobile@gmail.com>
2020-05-11 01:07:10 +0300

    commite one

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('hello')
+print ('введите число А')
+A = int(input())
+print ('введите число B')
+B = int(input())
+result = A * B
+if A * B <0:
+print ('Значение должны быть больше 0')
+else:
+print (f'умножение{result}')
+
+
+
+
+
+
+
+
