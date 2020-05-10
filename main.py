119ed4a2c4fae3880e4251c20f959126dc01a50b
safarmammadov <safarmobile@gmail.com>
2020-05-11 01:49:46 +0300

    commite two

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ print ('введите число А')
 A = int(input())
 print ('введите число B')
 B = int(input())
-result = A * B
-if A * B <0:
-print ('Значение должны быть больше 0')
+Mult = A * B
+Devis = A / B
+Summ = A + B
+Sub = A - B
+while  A * B <=0:
+    if A * B <0:
+        print ('Значение должны быть больше 0')
+        break
 else:
-print (f'умножение{result}')
+    print (f'сумма: {Summ}')
+    print (f'разница: {Sub}')
+
 
 
 
