d6353fac6e0c9a1a5ce441ac89541cb663bc58c1
safarmammadov <safarmobile@gmail.com>
2020-05-11 20:15:07 +0300

    commite from Alex

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-print ('введите число А')
-A = int(input())
-print ('введите число B')
-B = int(input())
-Mult = A * B
-Devis = A / B
-Summ = A + B
-Sub = A - B
-while  A * B <=0:
-    if A * B <0:
-        print ('Значение должны быть больше 0')
-        break
+print ('Привет')
+while True:
+        print ('введите число А')
+        A = int(input())
+        print ('введите число B')
+        B = int(input())
+        Mult = A * B
+        Devis = A / B
+        Summ = A + B
+        Sub = A - B
+        if A * B <= 0:
+                print('Значение должны быть больше 0')
+        else:
+                print(f'Умножение: {Mult}')
+                print(f'Деление: {Devis}')
+                break
 else:
-    print (f'Умножение: {Mult}')
-    print (f'Деление: {Devis}')
+        print('Значение должны быть больше 0')
